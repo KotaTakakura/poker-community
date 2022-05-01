7e4d7e74e833752e597e4ae6ee54e8ccfb8f2056
taka <>
2022-05-01 20:18:25 +0900

    修正

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,16 +1,27 @@
 import Link from 'next/link'
 import React, { useState } from 'react';
+import axios from "axios";
 
 export default function FirstPost() {
     const language_list: string[] = ['japanese','english']
     const default_language: string = language_list[0]
 
     const message_list: { [key:string] : string } = {
-        japanese: 'こんにちは、私はFirst Postです。',
+        japanese: 'こんにちは、asaaaaa私はFirst Postです。',
         english: 'Hello, I\'m First Post.',
     }
     const [selected_language, setSelectedLanguage] = useState(default_language)
     const [message, setMessage] = useState(message_list[selected_language]);
+
+    const url = process.env.NEXT_PUBLIC_API_BASE_URL
+    axios.get(url + '/ping')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        }).catch(function (error) {
+        // handle error
+        console.log(error);
+        })
 
     return (
         <>
