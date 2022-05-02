68c31d0c0e2bfae8ea9eebcb54924bcc9f4739e4
taka <>
2022-05-02 23:48:36 +0900

    カード確認

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,49 +1,24 @@
 import Link from 'next/link'
 import React, { useState } from 'react';
 import axios from "axios";
+import Card from '../../src/components/card/card'
 
 export default function FirstPost() {
-    const language_list: string[] = ['japanese','english']
-    const default_language: string = language_list[0]
-
-    const message_list: { [key:string] : string } = {
-        japanese: 'こんにちは、asaaaaa私はFirst Postです。',
-        english: 'Hello, I\'m First Post.',
-    }
-    const [selected_language, setSelectedLanguage] = useState(default_language)
-    const [message, setMessage] = useState(message_list[selected_language]);
-
-    const url = process.env.NEXT_PUBLIC_API_BASE_URL
-    axios.get(url + '/ping')
-        .then(function (response) {
-            // handle success
-            console.log(response);
-        }).catch(function (error) {
-        // handle error
-        console.log(error);
-        })
-
     return (
         <>
-            <FirstPostMessage message={message} />
-            <button onClick={() => {
-                setSelectedLanguage('japanese')
-                setMessage(message_list['japanese'])
-            }}>日本語</button>
-            <button onClick={() => {
-                setSelectedLanguage('english')
-                setMessage(message_list['english'])
-            }}>英語</button>
+            <Card number={'2'} suit={'heart'}></Card>
+            <Card number={'3'} suit={'diamond'}></Card>
+            <Card number={'4'} suit={'spade'}></Card>
+            <Card number={'5'} suit={'club'}></Card>
+            <Card number={'6'} suit={'heart'}></Card>
+            <Card number={'7'} suit={'diamond'}></Card>
+            <Card number={'8'} suit={'spade'}></Card>
+            <Card number={'9'} suit={'club'}></Card>
+            <Card number={'T'} suit={'heart'}></Card>
+            <Card number={'J'} suit={'diamond'}></Card>
+            <Card number={'Q'} suit={'spade'}></Card>
+            <Card number={'K'} suit={'club'}></Card>
+            <Card number={'A'} suit={'heart'}></Card>
         </>
-    )
-}
-
-interface PropsFirstPostMessage {
-    message: string,
-}
-
-function FirstPostMessage(props: PropsFirstPostMessage) {
-    return (
-        <h1>{props.message}</h1>
     )
 }
